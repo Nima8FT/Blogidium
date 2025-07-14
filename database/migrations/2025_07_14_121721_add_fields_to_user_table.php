4adaa64a4da51c6cca38e394503e0baf3a2d3aa4
Nima8FT <nima_8ak@yahoo.com>
2025-07-14 15:54:12 +0330

    add some fields like soft deletes to table users

---
+++ database/migrations/2025_07_14_121721_add_fields_to_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->unique();
+            $table->text('profile_photo')->nullable();
+            $table->string('phone')->nullable()->unique();
+            $table->timestamp('phone_verified_at')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['username','profile_photo','phone','phone_verified_at','is_active']);
+            $table->dropSoftDeletes();
+        });
+    }
+};
